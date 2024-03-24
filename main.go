3b05ac9083adb8ae4d72537b50dac9c4a121a2af
Jon Clarke <jonathan.clarke@holidayextras.com>
2024-03-24 00:23:31 +0000

    error message test

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func serveTemplate(w http.ResponseWriter, thisWeeksMeals MealList) {
 func main() {
 	// load env
 	if err := godotenv.Load(); err != nil {
-		fmt.Printf("Error loading .env file: %s\n", err)
+		fmt.Printf("Well, we have had an error loading .env file: %s\n", err)
 		return
 	}
 
